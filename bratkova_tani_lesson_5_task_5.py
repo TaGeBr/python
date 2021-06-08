9e08cd430cc8db97e0c6e52a85e0b7cb59e36b7d
tanjafox <btv_another@mail.ru>
2021-06-08 22:47:38 +0300

    Внесены изменения в задачи 4 и 5

--- bratkova_tani_lesson_5_task_5.py
+++ bratkova_tani_lesson_5_task_5.py
@@ -11,3 +11,7 @@ src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 result = []
 [result.append(num) for num in src if src.count(num) == 1]
 print(result)
+
+print('\nразбор ДЗ с преподавателем')
+src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+print([x for x in src if src.count(x) == 1])
